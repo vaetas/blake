a5b8d910ebf23128e4f4d23a4606d9e6b025f544
Vojtech Pavlovsky <vojtech.pavlovsky@gmail.com>
2020-11-04 13:49:25 +0100

    feat: measure elapsed time

--- lib/blake.dart
+++ lib/blake.dart
@@ -1,5 +1,6 @@
 import 'package:args/command_runner.dart';
 import 'package:blake/src/build.dart';
+import 'package:blake/src/cli.dart';
 import 'package:blake/src/init.dart';
 
 export 'src/markdown/parser.dart';
@@ -9,5 +10,12 @@ class Blake {
     ..addCommand(BuildCommand())
     ..addCommand(InitCommand());
 
-  void call(List<String> args) => _runner.run(args);
+  Future<int> call(List<String> args) async {
+    final stopwatch = Stopwatch()..start();
+    final exitCode = await _runner.run(args);
+    stopwatch.stop();
+
+    print('Done in ${stopwatch.elapsed.inMilliseconds} milliseconds.');
+    return exitCode;
+  }
 }
