abcca2c62710dde418a95918e5647796dbf9b913
Vojtech Pavlovsky <vojtech.pavlovsky@gmail.com>
2020-11-19 21:47:19 +0100

    refactor: build config constructor

--- lib/src/build/build_config.dart
+++ lib/src/build/build_config.dart
@@ -4,26 +4,35 @@ import 'package:yaml/yaml.dart';
 /// Configuration for building static site.
 class BuildConfig {
   const BuildConfig({
-    this.verbose = false,
-    this.publicFolder = 'public',
-    this.contentFolder = 'content',
-    this.templatesFolder = 'templates',
-    this.staticFolder = 'static',
-    this.dataFolder = 'data',
-  });
+    bool verbose = false,
+    String publicFolder,
+    String contentFolder,
+    String templatesFolder,
+    String staticFolder,
+    String dataFolder,
+    String typesFolder,
+  })  : verbose = verbose ?? false,
+        publicFolder = publicFolder ?? 'public',
+        contentFolder = contentFolder ?? 'content',
+        templatesFolder = templatesFolder ?? 'templates',
+        staticFolder = staticFolder ?? 'static',
+        dataFolder = dataFolder ?? 'data',
+        typesFolder = typesFolder ?? 'types';
 
   factory BuildConfig.fromYaml(YamlMap map) {
     assert(map != null);
     return BuildConfig(
-      verbose: map.get(_kVerbose, false),
-      publicFolder: map.get(_kPublicFolder, 'public'),
-      contentFolder: map.get(_kContentFolder, 'content'),
-      templatesFolder: map.get(_kTemplatesFolder, 'templates'),
-      staticFolder: map.get(_kStaticFolder, 'static'),
-      dataFolder: map.get(_kDataFolder, 'data'),
+      verbose: map.get<bool>(_kVerbose),
+      publicFolder: map.get<String>(_kPublicFolder),
+      contentFolder: map.get<String>(_kContentFolder),
+      templatesFolder: map.get<String>(_kTemplatesFolder),
+      staticFolder: map.get<String>(_kStaticFolder),
+      dataFolder: map.get<String>(_kDataFolder),
+      typesFolder: map.get<String>(_kTypesFolder),
     );
   }
 
+  /// Enable debug prints.
   final bool verbose;
 
   /// Folder with generated site files.
@@ -41,6 +50,8 @@ class BuildConfig {
   /// YAML/JSON data accessible for rendering
   final String dataFolder;
 
+  final String typesFolder;
+
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       _kVerbose: verbose,
@@ -49,6 +60,7 @@ class BuildConfig {
       _kTemplatesFolder: templatesFolder,
       _kStaticFolder: staticFolder,
       _kDataFolder: dataFolder,
+      _kTypesFolder: typesFolder,
     };
   }
 
@@ -62,3 +74,4 @@ const _kContentFolder = 'content_folder';
 const _kTemplatesFolder = 'templates_folder';
 const _kStaticFolder = 'static_folder';
 const _kDataFolder = 'data_folder';
+const _kTypesFolder = 'types_folder';
